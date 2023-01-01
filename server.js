d2718140f31f936ffcb251d2a8083176e3525e2e
xeos <syl9456@live.com>
2023-01-01 22:31:24 +0100

    Premier commit

---
+++ server.js
@@ -0,0 +1,64 @@
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+let assignment = require('./routes/assignments');
+
+let mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+//mongoose.set('debug', true);
+
+// remplacer toute cette chaine par l'URI de connexion à votre propre base dans le cloud
+const uri = 'mongodb+srv://sr:24062012@cluster0.3ojg5sg.mongodb.net/assignments?retryWrites=true&w=majority';
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify:false
+};
+
+// On se connecte à la base de données
+mongoose.connect(uri, options)
+  .then(() => {
+    console.log("Connecté à la base MongoDB assignments dans le cloud !");
+    console.log("at URI = " + uri);
+    console.log("vérifiez with http://localhost:8010/api_assignment/assignments que cela fonctionne")
+    },
+    err => {
+      console.log('Erreur de connexion: ', err);
+    });
+
+// Pour accepter les connexions cross-domain (CORS)
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
+// Pour les formulaires
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// les routes
+const prefix = '/api_assignment';
+
+app.route(prefix + '/assignments')
+  .get(assignment.getAssignments);
+
+app.route(prefix + '/assignments/:id')
+  .get(assignment.getAssignment)
+  .delete(assignment.deleteAssignment);
+
+app.route(prefix + '/assignments')
+  .post(assignment.postAssignment)
+  .put(assignment.updateAssignment);
+
+
+let port = process.env.PORT || 8010;
+// On démarre le serveur
+app.listen(port, "0.0.0.0");
+console.log('Serveur démarré sur http://localhost:' + port);
+
+module.exports = app;
+
+
