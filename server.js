04160c6924a5a8640a580653385819afd98a3e48
xeos <syl9456@live.com>
2023-01-02 17:32:49 +0100

    Nouveau commit

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 let express = require('express');
+const cors = require("cors");
 let app = express();
 let bodyParser = require('body-parser');
 let assignment = require('./routes/assignments');
@@ -6,6 +7,20 @@ let assignment = require('./routes/assignments');
 let mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 //mongoose.set('debug', true);
+
+
+var corsOptions = {
+  origin: "https://assignment-app-heroku.herokuapp.com"
+};
+
+// On autorise les connexions cross-domain (CORS)
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
 
 // remplacer toute cette chaine par l'URI de connexion à votre propre base dans le cloud
 const uri = 'mongodb+srv://sr:24062012@cluster0.3ojg5sg.mongodb.net/assignments?retryWrites=true&w=majority';
@@ -28,12 +43,12 @@ mongoose.connect(uri, options)
     });
 
 // Pour accepter les connexions cross-domain (CORS)
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   next();
+// });
 
 // Pour les formulaires
 app.use(bodyParser.urlencoded({extended: true}));
